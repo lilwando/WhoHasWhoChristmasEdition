d088d6c269068308947df2fb1ce30b16df74ebfd
lilwando <iwonawladzinska@onet.pl>
2022-12-01 15:33:44 +0100

    Pstro

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner consoleReader = new Scanner(System.in);
+        System.out.println("Wprowadz imiona biorące udział w losowaniu oddzielając je przecinkiem ");
+        String nameInput = consoleReader.nextLine();
+        String[] availableNames = nameInput.split(", ");
+        Random randomNumberGenerator = new Random();
+        List usedNumbers = new ArrayList();
+
+        for (int i=0; i < availableNames.length; i++) {
+            File plik = new File (availableNames[i] + ".txt");
+            int n = randomNumberGenerator.nextInt(availableNames.length);
+            while (usedNumbers.contains(n))
+            {
+                n = randomNumberGenerator.nextInt(availableNames.length);
+            }
+            usedNumbers.add(n);
+            PrintWriter zapis = new PrintWriter(availableNames[i] + ".txt");
+            zapis.println("Wylosowana osoba to " + availableNames[n]);
+            zapis.close();
+        }
+
+    }
+}
